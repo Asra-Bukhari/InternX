0d93c1a7e2dbb4a839a4f5d1fcd5cbf4190140bf
Asra <asrabukhari2003@gmail.com>
2026-07-01 07:53:04 +0500

    Made Changes for Deployment

--- Implementation/internx/backend/server.js
+++ Implementation/internx/backend/server.js
@@ -2,9 +2,12 @@ require("dotenv").config({ path: require("path").resolve(__dirname, ".env") });
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
-
 connectDB();
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+if (!process.env.VERCEL) {
+  app.listen(5000, () => {
+    console.log("Server running on port 5000");
+  });
+}
+
+module.exports = app;
